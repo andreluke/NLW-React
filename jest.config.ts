b6f5e7deec5c5d5ea75567b5978bd8ca0154ae3d
andreluke <andre.l.sales2706@gmail.com>
2025-03-03 16:49:59 -0300

    :wrench: test: Configuração estável dos testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,19 @@
 import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
 const config: Config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest', // Removido `useESM: true`
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.svg$': 'identity-obj-proxy',
   },
 }
 
-export default config
+export default createJestConfig(config)
